be9fbdb9ee8bc447e27859adced546c5ef4250f1
Vinh Nguyen <nguyenvq714@gmail.com>
2018-08-27 18:24:32 +0000

    version 0.0.2

--- inst/doc/Tutorial.R
+++ inst/doc/Tutorial.R
@@ -10,8 +10,8 @@ library(calpassapi)
 library(dplyr) # Ease in manipulations with data frames
 
 ## ---- eval=FALSE---------------------------------------------------------
-#  cp_token <- calpass_get_token(username='my_cp_api_uid', password='my_cp_api_pwd')
-#  # cp_token <- calpass_get_token() ## if cp_api_uid and cp_api_pwd are set in .Renviron
+#  cp_token <- calpass_get_token(username='my_cp_api_uid', password='my_cp_api_pwd', client_id='my_client_id', scope='my_scope')
+#  # cp_token <- calpass_get_token(client_id='my_client_id', scope='my_scope') ## if cp_api_uid and cp_api_pwd are set in .Renviron
 
 ## ------------------------------------------------------------------------
 # single
@@ -38,8 +38,18 @@ df %>%
 #  calpass_query(interSegmentKey=isk
 #              , token=cp_token, endpoint='transcript')
 #  ## multiple
-#  dfResults <- calpass_query(interSegmentKey=df$isk
+#  dfResults <- calpass_query_many(interSegmentKey=df$isk
 #                           , token=cp_token
 #                           , endpoint='transcript'
+#                             )
+
+## ---- eval=FALSE---------------------------------------------------------
+#  ## batches
+#  dfResults <- calpass_query_many(interSegmentKey=df$isk
+#                           , token=cp_token
+#                           , endpoint='transcript'
+#                           , api_call_limit=2 ## batches of 2
+#                           , limit_per_n_sec=10 ## every 10 seconds
+#                           , wait=TRUE
 #                             )
 
