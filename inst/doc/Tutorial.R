66bd2ae53975fd44608f8f43534e0d6912d8e515
Vinh Nguyen <nguyenvq714@gmail.com>
2020-08-06 14:30:02 +0000

    version 0.0.3

--- inst/doc/Tutorial.R
+++ inst/doc/Tutorial.R
@@ -25,13 +25,13 @@ gender <- c('Male', 'Female', 'X')
 birthdate <- c(20001231, 19990101, 19981111)
 df <- data.frame(firstname, lastname
                , gender, birthdate, stringsAsFactors=FALSE)
-df %>%
+df <- df %>%
   mutate(isk=calpass_create_isk(first_name=firstname
                               , last_name=lastname
                               , gender=gender
                               , birthdate
                                 ))
-
+df$isk
 
 ## ---- eval=FALSE---------------------------------------------------------
 #  ## single
@@ -41,6 +41,14 @@ df %>%
 #  dfResults <- calpass_query_many(interSegmentKey=df$isk
 #                           , token=cp_token
 #                           , endpoint='transcript'
+#                             )
+#  ## can specify credentials
+#  dfResults <- calpass_query_many(interSegmentKey=df$isk
+#                           , endpoint='transcript'
+#                           , token_username='my_username'
+#                           , token_password='my_password'
+#                           , token_client_id='my_client_id'
+#                           , token_scope='my_scope'
 #                             )
 
 ## ---- eval=FALSE---------------------------------------------------------
